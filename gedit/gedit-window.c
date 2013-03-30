0434e2f9188cb39b51408859e3e30a8a625b41c6
José Aliste <jaliste@src.gnome.org>
2013-03-30 00:03:44 -0300

    Do not present to the user hidden highlight languages

--- gedit/gedit-window.c
+++ gedit/gedit-window.c
@@ -1023,7 +1023,11 @@ get_languages_sorted_by_section (GeditWindow *window)
 		GtkSourceLanguage *lang;
 
 		lang = gtk_source_language_manager_get_language	(lm, ids[i]);
-		languages = g_slist_prepend (languages, lang);
+
+		if (!gtk_source_language_get_hidden (lang))
+		{
+			languages = g_slist_prepend (languages, lang);
+		}
 	}
 
 	return g_slist_sort (languages, (GCompareFunc)language_compare);
@@ -2088,19 +2092,22 @@ fill_language_combo (GeditWindow *window)
 		GtkSourceLanguage *lang;
 
 		lang = gtk_source_language_manager_get_language (lm, ids[i]);
+		
+		if (!gtk_source_language_get_hidden (lang))
+		{
+			name = gtk_source_language_get_name (lang);
+			menu_item = gtk_menu_item_new_with_label (name);
+			gtk_widget_show (menu_item);
 
-		name = gtk_source_language_get_name (lang);
-		menu_item = gtk_menu_item_new_with_label (name);
-		gtk_widget_show (menu_item);
+			g_object_set_data_full (G_OBJECT (menu_item),
+			                        LANGUAGE_DATA,
+			                        g_object_ref (lang),
+			                        (GDestroyNotify)g_object_unref);
 
-		g_object_set_data_full (G_OBJECT (menu_item),
-				        LANGUAGE_DATA,
-					g_object_ref (lang),
-					(GDestroyNotify)g_object_unref);
-
-		gedit_status_combo_box_add_item (GEDIT_STATUS_COMBO_BOX (window->priv->language_combo),
-						 GTK_MENU_ITEM (menu_item),
-						 name);
+			gedit_status_combo_box_add_item (GEDIT_STATUS_COMBO_BOX (window->priv->language_combo),
+			                                 GTK_MENU_ITEM (menu_item),
+			                                 name);
+		}
 	}
 }
 
