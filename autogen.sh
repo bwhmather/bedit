0b4d03017fe7ba2e5c7d33d364de49c611a6e6e9
Martin Blanchard <tchaik@gmx.com>
2018-09-23 13:13:11 +0100

    build: Migrate to gettext
    Recent gettext version can extract and merge back strings from and to
    various file formats, no need for  intltool anymore.
    
    https://wiki.gnome.org/Initiatives/GnomeGoals/GettextMigration
    
    https://gitlab.gnome.org/GNOME/gedit/issues/70

--- autogen.sh
+++ autogen.sh
@@ -13,12 +13,6 @@ if test -z $AUTORECONF; then
         exit 1
 fi
 
-INTLTOOLIZE=`which intltoolize`
-if test -z $INTLTOOLIZE; then
-        echo "*** No intltoolize found, please install the intltool package ***"
-        exit 1
-fi
-
 GNOMEDOC=`which yelp-build`
 if test -z $GNOMEDOC; then
         echo "*** The tools to build the documentation are not found,"
@@ -35,8 +29,7 @@ fi
 git submodule update --init --recursive
 
 gtkdocize || exit $?
-autopoint --force || exit $?
-AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
+autoreconf --force --install --verbose
 
 cd $OLDDIR
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
