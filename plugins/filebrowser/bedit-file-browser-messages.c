cdeec7ec36271b62d6be8a3ae62e4b364644fae6
Ben Mather <bwhmather@bwhmather.com>
2020-04-11 12:40:53 +0100

    Make license headers consistent with the rest of bedit

--- plugins/filebrowser/bedit-file-browser-messages.c
+++ plugins/filebrowser/bedit-file-browser-messages.c
@@ -1,12 +1,17 @@
 /*
  * bedit-file-browser-messages.c
+ * This file is part of Bedit.
+ *
+ * Copyright (C) 2020 - Ben Mather
+ *
+ * Based on gedit-file-browser-messages.c from Gedit.
  *
  * Copyright (C) 2006 - Jesse van den Kieboom <jesse@icecrew.nl>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
