03fef73ea3016df5e08346ea1af38ffcaf76f7b1
Ben Mather <bwhmather@bwhmather.com>
2020-10-29 22:18:19 +0000

    Prevent user from focusing file browser button

--- plugins/filebrowser/bedit-file-browser-window-activatable.c
+++ plugins/filebrowser/bedit-file-browser-window-activatable.c
@@ -463,6 +463,7 @@ static void bedit_file_browser_window_activatable_activate(
 
     priv->action_area_button = gtk_menu_button_new();
     gtk_widget_set_valign(GTK_WIDGET(priv->action_area_button), GTK_ALIGN_CENTER);
+    gtk_widget_set_can_focus(GTK_WIDGET(priv->action_area_button), FALSE);
 
     gtk_container_add(
         GTK_CONTAINER(priv->action_area_button), action_area_button_image
