33e405b5e13e13d553c0815fd4b7afd127714540
Ben Mather <bwhmather@bwhmather.com>
2020-12-19 18:17:29 +0000

    Hacking on search

--- plugins/filebrowser/bedit-file-browser-search-view.c
+++ plugins/filebrowser/bedit-file-browser-search-view.c
@@ -29,6 +29,8 @@ struct _BeditFileBrowserSearchView {
     GFile *virtual_root;
     gchar *query;
 
+    GCancellable *cancellable;
+
     guint enabled : 1;
 };
 
@@ -173,6 +175,8 @@ static void bedit_file_browser_search_view_class_finalize(
 static void bedit_file_browser_search_view_init(
     BeditFileBrowserSearchView *view
 ) {
+    view->cancellable = NULL;
+
     gtk_widget_init_template(GTK_WIDGET(view));
 
     view->list_store = gtk_list_store_new(
@@ -275,11 +279,364 @@ void bedit_file_browser_search_view_set_enabled(
         g_object_notify(G_OBJECT(view), "enabled");
     }
 }
+
 gboolean bedit_file_browser_search_view_get_enabled(
     BeditFileBrowserSearchView *view
 ) {
     return view->enabled ? TRUE : FALSE;
 }
+
+
+
+
+
+
+
+void bedit_file_browser_search(
+    GFile *root, gchar const *query,
+    GHashMap *dir_cache,
+    GCancellable *cancellable,
+    GCallback *callback,
+    gpointer argument,
+) {
+
+
+}
+
+
+bedit_file_browser_search_unref(BeditFileBrowserSearch)
+
+
+
+
+
+
+
+static void bedit_file_browser_dir_next_load_cb(
+    GFileEnumerator *enumerator, GAsyncResult *result,
+    Search *search
+) {
+    BeditFileBrowserDir *dir;
+    GError *error = NULL;
+    GList *files;
+
+    g_return_if_fail(G_IS_FILE_ENUMERATOR(enumerator));
+
+    files = g_file_enumerator_next_files_finish(
+        enumerator, result, &error
+    );
+
+    if (g_cancellable_is_cancelled(search->cancellable)) {
+        g_object_unref(enumerator);
+        g_list_free(files);
+        if (error != NULL) g_error_free(error);
+        search_unref(search);
+
+        return;
+    }
+    // Note that search does not hold a strong reference to the directory, so
+    // it is not safe to dereference this pointer until after the cancellable
+    // has been checked.
+    dir = context->dir;
+    g_return_if_fail(BEDIT_IS_FILE_BROWSER_DIR(dir));
+
+    if (error != NULL) {
+        // TODO
+        // bedit_file_browser_dir_set_error(dir, error);
+        // bedit_file_browser_dir_unload(dir);
+
+        g_error_free(error);
+        g_object_unref(enumerator);
+
+        check_finished(search);
+
+        return;
+    }
+
+    if (files == NULL) {
+        // We've reached the end of the enumerator and can close everything up.
+        // TODO mark as finished.
+        g_object_unref(enumerator);
+        check_finished(search);
+
+        return;
+    }
+
+    for (GList *cursor = files; cursor != NULL; cursor = cursor->next) {
+        GFile *file;
+        GFileInfo *info;
+        gchar const *name;
+
+        info = cursor->data;
+        name = g_file_info_get_name(info);
+        file = g_file_get_child(dir->file, name);
+
+        bedit_file_browser_dir_add_child(dir, file);
+    }
+
+    g_list_free(files);
+
+    g_file_enumerator_next_files_async(
+        enumerator, DIRECTORY_LOAD_ITEMS_PER_CALLBACK, G_PRIORITY_DEFAULT,
+        context->cancellable,
+        (GAsyncReadyCallback)bedit_file_browser_dir_next_load_cb,
+        context
+    );
+}
+
+static void bedit_file_browser_dir_begin_load_cb(
+    GFile *file, GAsyncResult *result, Search *context
+) {
+    BeditFileBrowserDir *dir;
+    GError *error = NULL;
+    GFileEnumerator *enumerator;
+
+    g_return_if_fail(G_IS_FILE(file));
+
+    enumerator = g_file_enumerate_children_finish(file, result, &error);
+
+    if (g_cancellable_is_cancelled(context->cancellable)) {
+        if (enumerator != NULL) g_object_unref(enumerator);
+        if (error) g_error_free(error);
+
+        return;
+    }
+    // Note that context does not hold a strong reference to the directory, so
+    // it is not safe to dereference this pointer until after the cancellable
+    // has been checked.
+    dir = context->dir;
+    g_return_if_fail(BEDIT_IS_FILE_BROWSER_DIR(dir));
+
+    if (error != NULL) {
+        // TODO
+        // bedit_file_browser_dir_set_error(dir, error);
+        // bedit_file_browser_dir_unload(dir);
+
+        g_error_free(error);
+        async_context_unref(context);
+
+        return;
+    }
+
+    g_return_if_fail(G_IS_FILE_ENUMERATOR(enumerator));
+
+    g_file_enumerator_next_files_async(
+        enumerator, DIRECTORY_LOAD_ITEMS_PER_CALLBACK, G_PRIORITY_DEFAULT,
+        context->cancellable,
+        (GAsyncReadyCallback)bedit_file_browser_dir_next_load_cb,
+        context
+    );
+}
+
+void bedit_file_browser_dir_refresh(BeditFileBrowserDir *dir) {
+    g_return_if_fail(BEDIT_IS_FILE_BROWSER_DIR(dir));
+
+    if (dir->context != NULL) {
+        g_cancellable_cancel(dir->context->cancellable);
+        async_context_unref(dir->context);
+    }
+    dir->context = async_context_new(dir);
+
+    // Set loading to true.
+
+    // Free the children.
+    while (TRUE) {
+        GHashTableIter child_iter;
+        GFile *child;
+
+        g_hash_table_iter_init(&child_iter, dir->children);
+        if (!g_hash_table_iter_next(&child_iter, (gpointer) &child, NULL)) {
+            break;
+        }
+
+        bedit_file_browser_dir_remove_child(dir, child);
+    }
+
+    g_file_enumerate_children_async(
+        dir->file, STANDARD_ATTRIBUTE_TYPES, G_FILE_QUERY_INFO_NONE,
+        G_PRIORITY_DEFAULT, dir->context->cancellable,
+        (GAsyncReadyCallback)bedit_file_browser_dir_begin_load_cb,
+        async_context_ref(dir->context)
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static void bedit_file_browser_search_view_refresh(
+    BeditFileBrowserSearchView *view
+) {
+    g_return_if_fail(BEDIT_IS_FILE_BROWSER_SEARCH_VIEW(view));
+
+    if (!bedit_file_browser_search_view_get_enabled(view)) {
+        return;
+    }
+
+    if (view->cancellable != NULL) {
+        g_cancellable_cancel(view->cancellable);
+        g_clear_object(&view->cancellable);
+    }
+    view->cancellable = g_cancellable_new();
+
+    if (view->dir_cache == NULL) {
+        view->dir_cache = g_hash_table_new_full(
+            g_file_hash, (GEqualFunc) g_file_equal,
+            g_object_unref, g_object_unref
+        );
+    }
+
+    do_search(
+        view->root, view->query,
+        dir_cache,
+        cancellable,
+        on_search_completed_cb,
+        g_weak_ref(view)
+    );
+
+    queue_process_dir(search, virtual_root);
+
+}
+
+
+
+
+
+
+
+
+
+
+class Dir:
+public:
+    property loaded
+
+
+private:
+    monitor
+    cancellable
+
+
+
+class DirCache:
+
+
+
+
+
+
+Expanding a directory adds its contents to the internal list of refs.
+
+
+
+
+
+
+def is_dir(GFile):
+    pass
+
+def is_hidden(GFile):
+    pass
+
+def is_binary(GFile):
+    pass
+
+
+
+info_cache: Dict[GFile, GFileInfo]
+dir_cache: Dict[GFile, List[GFileInfo]]
+
+
+
+
+
+
+def process_dir():
+    pending -= 1
+    for file in dir.files:
+        if match(file):
+            matches.add(file)
+
+        if is_dir(file) and match_partial(file):
+            queue_process_dir(file)
+
+    if pending == 0:
+        display()
+
+
+def queue_process_dir():
+    pending += 1
+    if dir in dir_cache:
+        process_dir(dir)
+    else:
+        list_dir(callback=process_dir)
+
+
+
+
+
+
+pending: Set[GFile]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void _bedit_file_browser_search_view_register_type(GTypeModule *type_module) {
     bedit_file_browser_search_view_register_type(type_module);
